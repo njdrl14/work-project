d848386773fc76cd4745c1ee0050b9b115969e24
doug <njdrl14@gmail.com>
2017-01-02 23:16:15 -0500

    added nodemon and updated code on index file

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,19 @@ module.exports = {
   context: __dirname,
   devtool: debug ? "inline-sourcemap" : null,
   entry: "./js/scripts.js",
+   module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      }
+    ]
+  },
   output: {
     path: __dirname + "/js",
     filename: "scripts.min.js"
