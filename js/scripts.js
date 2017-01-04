9f86176a8ecf4541f616b70bda087857367fa92e
doug <njdrl14@gmail.com>
2017-01-03 23:53:16 -0500

    .

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,17 @@
-require('./module1.js');
-require('./module2.js');
+import React from "react";
+import ReactDom from "react-dom";
+
+class Layout extends React.Component {
+	render() {
+		return (
+			<h1> It works! </h1>
+		);
+	}
+}
+
+const app = document.getElementById('app');
+ReactDOM.render(<Layout/>, app);
+		
+	
+
+
